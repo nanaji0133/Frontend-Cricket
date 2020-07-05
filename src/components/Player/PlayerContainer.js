b062e87d39efbf3ce595ab5992b03ffb0c07c86f
nanaji0133 <nanajisanka5357@gmail.com>
2020-07-04 21:35:33 -0400

    players list view updated with the names

--- src/components/Player/PlayerContainer.js
+++ src/components/Player/PlayerContainer.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 
@@ -7,24 +8,43 @@ class PlayerContainer extends Component
     {
         super();
         this.state = {
-
+            isLoaded: false,
+            teamName: "",
+            playerName: "" //player name is the user filed
         };
     }
 
     componentDidMount ()
     {
-        // axios.get("")
+        axios.all([axios.get(`http://127.0.0.1:8000/team/${this.props.team}`), axios.get(`http://127.0.0.1:8000/team/users/${this.props.user}`)])
+            .then(axios.spread((res1, res2) =>
+            {
+                this.setState({
+                    teamName: res1.data.team_name,
+                    playerName: res2.data.username,
+                    isLoaded: true
+                });
+                console.log(res1.data, res2.data);
+
+            }))
+            .catch(err => console.log(err));
     }
 
     render ()
     {
+
+        if (!this.state.isLoaded)
+        {
+            return null;
+        }
+        
         return (
             <div>
-                <h2>Player: { this.props.user } </h2>
+                <h2>Player: { this.state.playerName } </h2>
                 <ul>
                     <li>Country: { this.props.country } </li>
                     <li>Style: { this.props.style } </li>
-                    <li>Team: {this.props.team} </li>
+                    <li>Team: { this.state.teamName } </li>
                 </ul>
             </div>
         );
